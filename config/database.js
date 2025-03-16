7274b7f6d826a0e8489625404f02ca03c38aa43a
Guilherme Freitas <gui2010mg68@gmail.com>
2025-03-15 23:39:19 -0300

    corrigi erro sintatico

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const Pool = new Poll({
     user: process.env.DB_USER,
-    host: process.env,DB_HOST,
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.BD_PASS,
     port: process.env.BD_PORT
