9556dcc4bf0e52db8df4504e681b136882dbf577
Guilherme Freitas <gui2010mg68@gmail.com>
2025-03-23 13:50:43 -0300

    bolao

--- backend/src/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-const getUsers = (req, res) => {
-    res.send("Lista de usuários");
-};
-
-const createUser = (req, res) => {
-    res.send("Usuário criado");
-};
-
-module.exports = { getUsers, createUser };
